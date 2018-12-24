94af45f1ecc4c987efa40bdfd6f55b6e323547b6
Randolph Bailey <Randolph Bailey>
2018-12-24 08:41:18 -0600

    initial object definitions

---
+++ assets/javascript/game.js
@@ -0,0 +1,55 @@
+var obiwan = {
+    name: "Obi Wan",
+    healthPoints: 0,
+    attackPower: 0,
+    counterPower: 0
+}
+
+var yoda = {
+    name: "Yoda",
+    healthPoints: 0,
+    attackPower: 0,
+    counterPower: 0
+}
+
+var rey = {
+    name: "Rey",
+    healthPoints: 0,
+    attackPower: 0,
+    counterPower: 0
+}
+
+var darthvader = {
+    name: "Darth Vader",
+    healthPoints: 0,
+    attackPower: 0,
+    counterPower: 0
+}
+
+var kyloren = {
+    name: "Kylo Ren",
+    healthPoints: 0,
+    attackPower: 0,
+    counterPower: 0
+}
+
+var macewindu = {
+    name: "Mace Windu",
+    healthPoints: 0,
+    attackPower: 0,
+    counterPower: 0
+}
+
+var finn = {
+    name: "Finn",
+    healthPoints: 0,
+    attackPower: 0,
+    counterPower: 0
+}
+
+var r2d2 = {
+    name: "R2-D2",
+    healthPoints: 0,
+    attackPower: 0,
+    counterPower: 0
+}
