033b8cce39f1a895749150415568f5c9026cb405
randolphbailey <randolphbailey@gmail.com>
2019-01-08 18:04:05 -0600

    remove excess resources

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,36 +12,8 @@ var yoda = {
     counterPower: 0
 }
 
-var rey = {
-    name: "Rey",
-    healthPoints: 0,
-    attackPower: 0,
-    counterPower: 0
-}
-
 var darthvader = {
     name: "Darth Vader",
-    healthPoints: 0,
-    attackPower: 0,
-    counterPower: 0
-}
-
-var kyloren = {
-    name: "Kylo Ren",
-    healthPoints: 0,
-    attackPower: 0,
-    counterPower: 0
-}
-
-var macewindu = {
-    name: "Mace Windu",
-    healthPoints: 0,
-    attackPower: 0,
-    counterPower: 0
-}
-
-var finn = {
-    name: "Finn",
     healthPoints: 0,
     attackPower: 0,
     counterPower: 0
